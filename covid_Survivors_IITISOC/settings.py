77a033b8e7b6808b20366efe65cf6befea5e9ba1
bhavishya <bhavishyatiwari2000@gmail.com>
2021-07-26 04:34:37 +0530

    rev

--- covid_Survivors_IITISOC/settings.py
+++ covid_Survivors_IITISOC/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ok--6$1nadmzzoeetop7g0(_+n-w%*x8k#e=!y54#b37xka8__'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["covid-survivors.herokuapp.com", "localhost"]
 
 
 # Application definition
@@ -136,8 +136,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'covidsurvivorsiitisoc@gmail.com'
-EMAIL_HOST_PASSWORD ='henryisking'
+EMAIL_HOST_USER = os.environ['website_email']
+EMAIL_HOST_PASSWORD = os.environ['w_e_pass']
+
 MESSAGE_TAGS = {
     messages.ERROR:'danger'
 }

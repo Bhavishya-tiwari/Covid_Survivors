bd46db7faec93860ed0378b2e86d9c84d841457d
bhavishya <bhavishyatiwari2000@gmail.com>
2021-07-26 19:50:09 +0530

    to whited

--- covid_Survivors_IITISOC/settings.py
+++ covid_Survivors_IITISOC/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # # STATIC_ROOT=BASE_DIR / 'static'
@@ -148,4 +148,4 @@ MESSAGE_TAGS = {
     messages.ERROR:'danger'
 }
 django_heroku.settings(locals())
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'

fe7264313e1460b1fbe8b240eca118473b71db01
bhavishya <bhavishyatiwari2000@gmail.com>
2021-07-29 00:01:58 +0530

    sf

--- covid_Survivors_IITISOC/settings.py
+++ covid_Survivors_IITISOC/settings.py
@@ -24,10 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'django-insecure-ok--6$1nadmzzoeetop7g0(_+n-w%*x8k#e=!y54#b37xka8__'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ['DEBUG']
+DEBUG = True
 ALLOWED_HOSTS = ['covid-survivorsiitisoc.herokuapp.com','127.0.0.1']
 
 
@@ -80,7 +81,12 @@ WSGI_APPLICATION = 'covid_Survivors_IITISOC.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = os.environ['DATA_BASE']
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
@@ -143,9 +149,10 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'bhavishyat24@gmail.com'
-EMAIL_HOST_PASSWORD =os.environ['EPASS']
+EMAIL_HOST_PASSWORD ='henryisking'
 MESSAGE_TAGS = {
     messages.ERROR:'danger'
 }
 django_heroku.settings(locals())
 # STATIC_ROOT = BASE_DIR / 'staticfiles'
+

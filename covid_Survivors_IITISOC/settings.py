af820922782369f9f3b4ef62fa1352fe0ef263f3
bhavishya <bhavishyatiwari2000@gmail.com>
2021-08-09 13:58:49 +0530

    n

--- covid_Survivors_IITISOC/settings.py
+++ covid_Survivors_IITISOC/settings.py
@@ -15,13 +15,8 @@ import os
 from pathlib import Path
 from django.contrib.messages import constants as messages
 
-import environ
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
+
 # reading .env file
-environ.Env.read_env()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
